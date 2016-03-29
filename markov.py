140c81509824c5919375f005e7e60ddb9dea9c77
oshlern <oshlern@nuevaschool.org>
2016-03-28 17:31:52 -0700

    Fixed data getting of lines, words, and speakers. Starting on the markov chain and speechLengths

--- markov.py
+++ markov.py
@@ -3,10 +3,10 @@ import re, operator
 # First Chain: Who is talking - {Othello: {2: Desdemona, 18: Desdemona, ...}, Iago.}
 
 # should line num be half-character
-# null: {1: Let}, Let: {1: him}, him, {}
+# ~null: {1: Let}, Let: {1: him}, him, {}
 # Range: 1 <= x > x+1/3x
-# speakers = {'null': {}, 'Othello': {2: 'Iago', 18: 'Desdemona', ...}, 'Iago': {}}
-# speeches = {'Othello': {'lines': {12: 1, 23: 2, 68: 3}, 'words': {'null': {}, 'hi': {'.': 12, 'there'``}, '.':  }}}
+# speakers = {'~null': {}, 'Othello': {2: 'Iago', 18: 'Desdemona', ...}, 'Iago': {}}
+# speeches = {'Othello': {'lines': {12: 1, 23: 2, 68: 3}, 'words': {'~null': {}, 'hi': {'.': 12, 'there'``}, '.':  }}}
 
 def findTotal(weights):
     total = 0
@@ -15,20 +15,28 @@ def findTotal(weights):
             total += weight
     return total
 def pickItem(lastItem):
-    total = findTotal(lastItem)
+    total = findTotal(lastItem.items())
     rand = random.random()*total
-    for weight in lastItem:
-        if rand<weight:
-            return lastItem[weight]
-        rand -= weight
+    for item in lastItem:
+        if rand<lastItem[item]:
+            return item
+        rand -= lastItem[item]
 def printSpeaker(speaker):
     return speaker + ':\n'
-def makeSpeech(speaker):
-    word = speaker['null']
+def printWord(word):
+    if word == r'[^\w\']':
+        return word
+    return ' ' + word
+def printLine():
+    return '\n'
+def makeLine(words, lineLength):
+    word = '~null'
+def makeSpeech(words, lineLengths, speechLength):
+    word = '~null'
     wordNum = pickItem
-def run():
+def makeDialogue(words, lineLengths, speechLengths, speakers):
     text = ''
-    speaker = pickItem(speakers['null'])
+    speaker = pickItem(speakers['~null'])
     for speech in range(100):
         text += printSpeaker(speaker)
         speaker = speakers[speaker]
@@ -50,7 +58,7 @@ def run():
 form = {
     'act': r'Act \d+:\n',
     'scene': r'Scene \d+:\n',
-    'speaker': r'(\w+):\n',
+    'speaker': r'\n([a-z1-9]+):\n',
     'line': r'([^\n]+)\n',
     'stage': r'\[(.*)\]'
 }
@@ -63,51 +71,54 @@ def openData(doc):
 
 # Make lastWord and lastWordNum speaker specific
 def textParse(text, form):
-    speakers = {'null': {}, 'last': 'null'}
-    speeches = {}
-    lines = {}
-    text = re.sub(form['act'] or form['scene'] or form['stage'], '', text)
-    text = re.sub(form['speaker'], r'|speaker|\1|lines|', text)
-    speechesText = text.split('|speaker|')[1:]
-    for speech in speechesText:
+    speakers = {'~null': {}, '~last': '~null'}
+    words = {}
+    speechLengths = {}
+    lineLengths = {}
+    # text = re.sub(form['act'] or form['scene'] or form['stage'] or 'xxx:', '', text)
+    text = re.sub(form['act'], '', text)
+    text = re.sub(form['scene'], '', text)
+    text = re.sub(form['stage'], '', text)
+    text = re.sub('xxx:', '', text)
+    text = re.sub('\n\n\n', '\n', text)
+    text = re.sub(form['speaker'], r'\n|speaker|\1|lines|', text)
+    speeches = text.split('|speaker|')[1:]
+    for speech in speeches:
         speakerAndLines = speech.split('|lines|')
         speaker = speakerAndLines[0]
-        print speaker
-        linesText = speakerAndLines[1].split('\n')[:-1]
-        for superset in [speeches, lines]:
+        lines = speakerAndLines[1].split('\n')[:-1]
+        for superset in [words, lineLengths]:
             if not speaker in superset:
-                superset[speaker] = {'last': 'null'}
+                superset[speaker] = {'~null': {}, '~last': '~null'}
         if not speaker in speakers:
-            superset[speaker] = {}
-        # print speakers['last']
-        if not speaker in speakers[speakers['last']]:
-            speakers[speakers['last']][speaker] = 0
-        speakers[speakers['last']][speaker] += 1
-        for line in linesText:
-            # words = re.sub('[^a-zA-z-\' ]|\[|\]|--', '', line)
-            words = re.sub('([^\'])\b([^\'])', r'\1|break|\2', line)
-            words = re.sub(' ', '', words)
-            words = words.lower()
-            words = words.split('|break|')
+            speakers[speaker] = {}
+        if not speaker in speakers[speakers['~last']]:
+            speakers[speakers['~last']][speaker] = 0
+        speakers[speakers['~last']][speaker] += 1
+        for line in lines:
+            # chars = re.sub(r'([a-zA-z-\'])([^a-zA-z-\'])', r'\1', line)
+            chars = re.sub(r'([^\'])\b([^\'])', r'\1|break|\2', line)
+            chars = re.sub(' ', '', chars)
+            chars = chars.lower()
+            chars = chars.split('|break|')
             wordNum = 0
-            for word in words:
-                if r'[^ ]' in word:
-                    word = re.sub(' ', '', word)
-                    if not word in speeches[speaker]:
-                        speeches[speaker][word] = {}
-                    if not word in speeches[speaker][speeches[speaker]['last']]:
-                        speeches[speaker][speeches[speaker]['last']][word] = 0
-                    speeches[speaker][speeches[speaker]['last']][word] += 1
-                    speeches[speaker]['last'] = word
-                    wordNum += 1
-            if not wordNum in lines[speaker]:
-                lines[speaker][wordNum] = {}
-            if not wordNum in lines[speaker][lines[speaker]['last']]:
-                lines[speaker][lines[speaker]['last']][wordNum] = 0
-            lines[speaker][lines[speaker]['last']][wordNum] += 1
-            lines[speaker]['last'] = wordNum
-        speakers['last'] = speaker
-    return {'speakers': speakers, 'speeches': speeches, 'lines': lines}
+            for word in chars:
+                if not word in words[speaker]:
+                    words[speaker][word] = {}
+                if not word in words[speaker][words[speaker]['~last']]:
+                    words[speaker][words[speaker]['~last']][word] = 0
+                words[speaker][words[speaker]['~last']][word] += 1
+                words[speaker]['~last'] = word
+                wordNum += 1
+            if not wordNum in lineLengths[speaker]:
+                lineLengths[speaker][wordNum] = {}
+            if not wordNum in lineLengths[speaker][lineLengths[speaker]['~last']]:
+                lineLengths[speaker][lineLengths[speaker]['~last']][wordNum] = 0
+            lineLengths[speaker][lineLengths[speaker]['~last']][wordNum] += 1
+            lineLengths[speaker]['~last'] = wordNum
+        speakers['~last'] = speaker
+    return {'speakers': speakers, 'words': words, 'lines': lineLengths}
 
 plaintext = openData('text')
 text = textParse(plaintext, form)
+# print text['words']['iago']['is']
--- othello2.py
+++ othello2.py
@@ -150,6 +150,7 @@ words = speakerInfo(text['speakers']['iago'])['chars']
 # words = text['speakers']['iago']['words']
 largest = [0, '']
 
+print text['speakers'].keys()
 # print sorted(words.items(), key=operator.itemgetter(1))
 # https://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value
 
